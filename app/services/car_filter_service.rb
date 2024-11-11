00f11fbc536ef6b8a4f5f4c79cb1d8ccae92979c
pl1tz <pl1tz2021@gmail.com>
2024-11-11 14:52:11 +0300

    fix/ total page

--- app/services/car_filter_service.rb
+++ app/services/car_filter_service.rb
@@ -24,5 +24,17 @@ class CarFilterService
     cars
   end
 
+  def total_pages
+    total_cars = call.count
+    (total_cars.to_f / @per_page).ceil
+  end
+
+  def cars_count
+    call.count
+  end
+
+  def all_cars
+    call.to_a
+  end
  
 end
