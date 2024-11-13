ffe321a61ee8e4128fb21157ad450abe36113658
pl1tz <pl1tz2021@gmail.com>
2024-11-13 15:16:30 +0300

    fix/ doc 10

--- app/serializers/credit_serializer.rb
+++ app/serializers/credit_serializer.rb
@@ -1,3 +1,6 @@
 class CreditSerializer < ActiveModel::Serializer
   attributes :id, :car_id, :name, :phone, :credit_term, :initial_contribution, :banks_id, :programs_id
+  has_one :car
+  has_one :bank
+  has_one :program
 end
