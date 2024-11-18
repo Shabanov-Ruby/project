09c0cfa53b12cb38e497d71db4f643345a0e5aa6
pl1tz <pl1tz2021@gmail.com>
2024-11-18 17:24:19 +0300

    edit favorites 4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,14 @@
 class FavoritesController < ApplicationController
   def index
-    render file: "#{Rails.root}/public/index.html", layout: false
+    @favorites = get_favorites_from_storage
+    response.headers['Cache-Control'] = 'public, max-age=3600'
+    render json: @favorites
+  end
+
+  private
+
+  def get_favorites_from_storage
+    []
   end
 end
 
