eeddfb8b97c536ac90b03923328e9a25a0104198
pl1tz <pl1tz2021@gmail.com>
2024-11-19 22:17:37 +0300

    del cors localhost add host datebase

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', 'https://usecar.ru'# Укажите ваш фронтенд URL
+      origins 'https://usecar.ru'# Укажите ваш фронтенд URL
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
